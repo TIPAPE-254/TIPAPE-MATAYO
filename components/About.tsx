0aa9950d4505adea7548d518d7a4e0458bfa19a1
elvis kiptoo <elvis@kali.kali>
2026-01-18 14:07:16 -0500

    added CNAME

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PORTFOLIO_DATA } from '../constants';
+import image1 from '../assets/image1.webp';
 
 const About: React.FC = () => {
   return (
@@ -10,7 +11,7 @@ const About: React.FC = () => {
              <div className="relative group">
                 <div className="absolute -inset-1 bg-gradient-to-r from-primary to-secondary rounded-2xl blur opacity-25 group-hover:opacity-75 transition duration-1000 group-hover:duration-200"></div>
                 <img 
-                  src="/assets/image1.webp" 
+                  src={image1} 
                   alt="Tipape Matayo" 
                   className="relative rounded-2xl w-full object-cover shadow-2xl border border-gray-800"
                 />
